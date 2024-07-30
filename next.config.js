1f8d2b83ecfbc7451fc28628772d47a3877fce28
Alex Nogueira Martins <alex.martins@sankhya.com.br>
2024-07-30 10:27:17 -0300

    teste

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
-module.exports = {
-    async redirects() {
-      return [
-        {
-          source: '/',
-          destination: 'https://play.google.com/store/apps/details?id=br.com.sankhya.labs.rh&hl=pt_BR',
-          permanent: true,
-        },
-      ]
-    },
-  }
+// module.exports = {
+//     async redirects() {
+//       return [
+//         {
+//           source: '/',
+//           destination: 'https://play.google.com/store/apps/details?id=br.com.sankhya.labs.rh&hl=pt_BR',
+//           permanent: true,
+//         },
+//       ]
+//     },
+//   }
