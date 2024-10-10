3ca45288666ede4cd24ef3bc0b8cd6f3b7628feb
Alex Nogueira Martins <alex.martins@sankhya.com.br>
2024-10-10 10:24:38 -0300

    safari

--- next.config.js
+++ next.config.js
@@ -1,11 +1,23 @@
 module.exports = {
-    async redirects() {
-      return [
-        {
-          source: '/',
-          destination: 'https://play.google.com/store/apps/details?id=br.com.sankhya.labs.rh&hl=pt_BR',
-          permanent: true,
-        },
-      ]
-    },
-  }
+  async redirects() {
+    return [
+      {
+        source: '/',
+        has: [
+          {
+            type: 'header',
+            key: 'user-agent',
+            value: '.*Safari.*' // Detecção de Safari no user-agent
+          }
+        ],
+        destination: 'https://apps.apple.com/br/app/sankhya-rh/id123456789', // Link para Apple Store
+        permanent: true,
+      },
+      {
+        source: '/',
+        destination: 'https://play.google.com/store/apps/details?id=br.com.sankhya.labs.rh&hl=pt_BR', // Link para Google Play
+        permanent: true,
+      },
+    ]
+  },
+};
