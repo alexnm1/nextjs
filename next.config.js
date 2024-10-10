1b44456919649f8a04b480e6d0ed4fb4ca69cd26
Alex Nogueira Martins <alex.martins@sankhya.com.br>
2024-10-10 10:22:48 -0300

    back

--- next.config.js
+++ next.config.js
@@ -1,20 +1,11 @@
-import { useRouter } from 'next/router';
-import { useEffect } from 'react';
-
-const isIOS = () => {
-  return /iPad|iPhone|iPod/.test(navigator.userAgent);
-};
-
-export default function Home() {
-  const router = useRouter();
-
-  useEffect(() => {
-    const destination = isIOS()
-      ? 'https://apps.apple.com/br/app/seu-app/id123456789'
-      : 'https://play.google.com/store/apps/details?id=br.com.sankhya.labs.rh&hl=pt_BR';
-
-    router.push(destination, undefined, { shallow: true });
-  }, []);
-
-  return null; // Não renderizar nada, apenas o redirecionamento
-}
+module.exports = {
+    async redirects() {
+      return [
+        {
+          source: '/',
+          destination: 'https://play.google.com/store/apps/details?id=br.com.sankhya.labs.rh&hl=pt_BR',
+          permanent: true,
+        },
+      ]
+    },
+  }
