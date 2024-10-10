5ec505faaef39b3736132c700860b2fe28fe0e07
Alex Nogueira Martins <alex.martins@sankhya.com.br>
2024-10-10 10:05:49 -0300

    tff

--- next.config.js
+++ next.config.js
@@ -1,18 +1,24 @@
-const isIOS = () => {
-  // Função simples para detectar iOS (pode ser mais robusta em cenários reais)
-  return /iPad|iPhone|iPod/.test(navigator.userAgent);
-};
+import { useRouter } from 'next/router';
 
-module.exports = {
-  async redirects() {
-    return [
-      {
-        source: '/',
-        destination: isIOS()
-          ? 'https://apps.apple.com/br/app/seu-app/id123456789' // Substitua por seu ID da App Store
-          : 'https://play.google.com/store/apps/details?id=br.com.sankhya.labs.rh&hl=pt_BR',
-        permanent: true,
-      },
-    ];
-  },
-};
+export default function Home() {
+  const router = useRouter();
+
+  const handleRedirect = () => {
+    const isIOS = () => {
+      // ... mesma função isIOS() de antes
+    };
+
+    const destination = isIOS()
+      ? 'https://apps.apple.com/br/app/seu-app/id123456789'
+      : 'https://play.google.com/store/apps/details?id=br.com.sankhya.labs.rh&hl=pt_BR';
+
+    router.push(destination, undefined, { shallow: true });
+  };
+
+  // Chamar handleRedirect() ao carregar a página
+  useEffect(() => {
+    handleRedirect();
+  }, []);
+
+  return null; // Não renderizar nada, apenas o redirecionamento
+}
