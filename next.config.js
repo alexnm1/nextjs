0c7dee0f75b14800ce239bace1daf655222d67bd
Alex Nogueira Martins <alex.martins@sankhya.com.br>
2024-10-10 09:49:03 -0300

    t4

--- next.config.js
+++ next.config.js
@@ -4,27 +4,20 @@ module.exports = {
       {
         source: '/',
         destination: '/',
-        async has(req, res) {
-          // Captura o user-agent do cabeçalho da requisição
-          const userAgent = req.headers['user-agent'] || '';
-          
-          // Loga o user-agent para inspecionar no terminal
-          console.log('User-Agent:', userAgent);
-
-          // Lógica de redirecionamento com base no user-agent
-          if (/iPhone|iPad|Macintosh|Mac OS X/.test(userAgent)) {
-            return {
-              destination: 'https://apps.apple.com/br/app/sankhya-pessoas/id1317417474',
-              permanent: false,
-            };
-          }
-
-          return {
-            destination: 'https://play.google.com/store/apps/details?id=br.com.sankhya.labs.rh&hl=pt_BR',
-            permanent: false,
-          };
-        },
-        permanent: false,
+        has: [
+          {
+            type: 'header',
+            key: 'user-agent',
+            value: '(.*iPhone|.*iPad)',
+          },
+        ],
+        destination: 'https://apps.apple.com/br/app/sankhya-pessoas/id1317417474',
+        permanent: true,
+      },
+      {
+        source: '/',
+        destination: 'https://play.google.com/store/apps/details?id=br.com.sankhya.labs.rh&hl=pt_BR',
+        permanent: true,
       },
     ];
   },
