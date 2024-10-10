429ec06b6712df9c18a3be17cc40c070498723a8
Alex Nogueira Martins <alex.martins@sankhya.com.br>
2024-10-10 09:58:08 -0300

    t3

--- next.config.js
+++ next.config.js
@@ -3,12 +3,11 @@ module.exports = {
     return [
       {
         source: '/',
-        destination: '/',
         has: [
           {
             type: 'header',
             key: 'user-agent',
-            value: '(.*iPhone|.*iPad)',
+            value: '.*(iPhone|iPad).*',
           },
         ],
         destination: 'https://apps.apple.com/br/app/sankhya-pessoas/id1317417474',
@@ -16,6 +15,13 @@ module.exports = {
       },
       {
         source: '/',
+        has: [
+          {
+            type: 'header',
+            key: 'user-agent',
+            value: '.*Android.*',
+          },
+        ],
         destination: 'https://play.google.com/store/apps/details?id=br.com.sankhya.labs.rh&hl=pt_BR',
         permanent: true,
       },
