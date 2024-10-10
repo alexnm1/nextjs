1390c5ee24d15dde1c7e344983e4f585a1987dc7
Alex Nogueira Martins <alex.martins@sankhya.com.br>
2024-10-10 10:01:21 -0300

    t4

--- next.config.js
+++ next.config.js
@@ -1,30 +1,9 @@
-module.exports = {
-  async redirects() {
-    return [
-      {
-        source: '/',
-        has: [
-          {
-            type: 'header',
-            key: 'user-agent',
-            value: '.*(iPhone|iPad).*',
-          },
-        ],
-        destination: 'https://apps.apple.com/br/app/sankhya-pessoas/id1317417474',
-        permanent: true,
-      },
-      {
-        source: '/',
-        has: [
-          {
-            type: 'header',
-            key: 'user-agent',
-            value: '.*Android.*',
-          },
-        ],
-        destination: 'https://play.google.com/store/apps/details?id=br.com.sankhya.labs.rh&hl=pt_BR',
-        permanent: true,
-      },
-    ];
-  },
-};
+useEffect(() => {
+  const userAgent = navigator.userAgent || navigator.vendor || window.opera;
+
+  if (/iPhone|iPad|iPod/i.test(userAgent)) {
+    window.location.href = "https://apps.apple.com/br/app/sankhya-pessoas/id1317417474";
+  } else if (/Android/i.test(userAgent)) {
+    window.location.href = "https://play.google.com/store/apps/details?id=br.com.sankhya.labs.rh&hl=pt_BR";
+  }
+}, []);
