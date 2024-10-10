54aa63ebd7ef1ff41e14f60705bf07ba160c1d3a
Alex Nogueira Martins <alex.martins@sankhya.com.br>
2024-10-10 09:27:31 -0300

    teste no redirect pra appleStore

--- next.config.js
+++ next.config.js
@@ -1,11 +1,24 @@
 module.exports = {
-    async redirects() {
-      return [
-        {
-          source: '/',
-          destination: 'https://play.google.com/store/apps/details?id=br.com.sankhya.labs.rh&hl=pt_BR',
-          permanent: true,
-        },
-      ]
-    },
-  }
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/',
+        has: [
+          {
+            type: 'header',
+            key: 'user-agent',
+            value: '(.*iPhone|.*iPad)', // Regex para detectar iOS
+          },
+        ],
+        destination: 'https://apps.apple.com/br/app/sankhya-pessoas/id1317417474',
+        permanent: true,
+      },
+      {
+        source: '/',
+        destination: 'https://play.google.com/store/apps/details?id=br.com.sankhya.labs.rh&hl=pt_BR',
+        permanent: true,
+      },
+    ];
+  },
+};
